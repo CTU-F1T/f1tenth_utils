3a00076a1e936d437880d5655abe660e18f635a9
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2024-05-08 17:09:23 +0200

    plot_gg: Add second circle as well

--- plot_gg.py
+++ plot_gg.py
@@ -27,6 +27,11 @@ from sensor_msgs.msg import Imu
 # Global variables
 G = 9.81
 MU = 0.25
+RO = 1.2
+A = 0.3
+CL = 1.0
+M = 3.68
+VLIM = 4.5
 
 figure = None
 line = None
@@ -109,7 +114,13 @@ if __name__ == "__main__":
         radius = (MU * G),  # x**2 + y**2 <= R**2
         ls = "--", facecolor = 'r', edgecolor = 'k', alpha = 0.25, lw = 2.0
     )
+    limit2 = pyplot.Circle(
+        xy = (0, 0),
+        radius = MU * (G + 0.5 * RO * A * CL * VLIM**2 / M),
+        ls = "--", facecolor = 'g', edgecolor = 'k', alpha = 0.25, lw = 2.0
+    )
     figure.gca().add_patch(limit)
+    figure.gca().add_patch(limit2)
     figure.gca().set_aspect("equal")
     figure.gca().set_title("g-g diagram")
     figure.gca().set_xlabel("lateral acceleration [m.s^{-2}]")
